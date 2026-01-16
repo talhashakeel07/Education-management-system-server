c41da850e939d0bade080f1a5289932dd5cf3852
talhashakeel07 <talhashakeel0702@gmail.com>
2026-01-16 10:52:30 +0500

    DB added

--- DB/connect.js
+++ DB/connect.js
@@ -1,37 +1,15 @@
-// const mongoose = require('mongoose')
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/education_platform',{
-//              useNewUrlParser: true, useUnifiedTopology: true
-//         })
-//         console.log('MongoDB Connected Successfully')
-//     } catch (error) {
-//         console.log(`ERROR IN DB Connection: ${error}`)
-       
-//     }
-// }
-
-// module.exports = connectDB
-
-
-
-
-
-
-
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Direct local address use karein testing ke liye
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/education_platform');
+        const dbURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/education_platform';
+        
+        const conn = await mongoose.connect(dbURI);
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`DB Connection Failed: ${error.message}`);
-        process.exit(1); // Agar DB connect na ho to server band ho jaye
+        process.exit(1); 
     }
 };
 
